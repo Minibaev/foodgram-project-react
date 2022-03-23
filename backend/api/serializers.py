cc6c67ecaf14e6eeab792b819efcaeda43138c3d
Aidar <minibaev662@gmail.com>
2022-03-23 23:54:26 +0500

    v15

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -78,7 +78,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                                        recipe=obj).exists()
 
     def get_ingredients_amount(self, ingredients, recipe, data):
-        tags = data['tags']
+        tags = self.data['tags']
         for tag_id in tags:
             recipe.tags.add(get_object_or_404(Tag, pk=tag_id))
         for ingredient in ingredients:
@@ -90,7 +90,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             ingredients_amount.save()
 
     def validate(self, data):
-        ingredients = data['ingredients']
+        ingredients = self.data['ingredients']
         ingredients_set = set()
         for ingredient in ingredients:
             if int(ingredient.get('amount')) <= 0:
