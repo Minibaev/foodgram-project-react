9877095e18b9e5eb1c42a645f4f65b2255254dd8
Aidar <minibaev662@gmail.com>
2022-03-25 02:12:42 +0500

    v62

--- backend/api/views.py
+++ backend/api/views.py
@@ -93,11 +93,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     pagination_class = CustomPagination
     permission_classes = (IsOwnerOrAdminOrReadOnly,)
     filter_class = RecipeFilter
+    serializer_class = CreateUpdateRecipeSerializer
 
-    def get_serializer_class(self):
-        if self.action == 'create' or self.action == 'update':
-            return CreateUpdateRecipeSerializer
-        return ListRecipeSerializer
+    #def get_serializer_class(self):
+        #if self.action == 'create' or self.action == 'update':
+        #return CreateUpdateRecipeSerializer
+        #return ListRecipeSerializer
 
     def perform_create(self, serializer):
         return serializer.save(author=self.request.user)
